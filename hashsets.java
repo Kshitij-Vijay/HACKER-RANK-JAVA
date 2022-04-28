dd9abf4cd456a6ceed70d661d5f5da4b54f2def0
XITIJ <97301437+Kshitij-Vijay@users.noreply.github.com>
2022-04-28 22:13:00 +0530

    just font adjustments

--- hashsets.java
+++ hashsets.java
@@ -9,10 +9,7 @@ public class hashsets {
         String[] pair_left = { "jhon", "jhon", "jhon", "mary", "mary" };
         String[] pair_right = { "tom", "mary", "tom", "anna", "anna" };
         
-        /*
-            https://www.hackerrank.com/challenges/java-hashset/problem?isFullScreen=true
-        */
-        
+               
         HashSet <String> h = new HashSet<>();
         for(int i = 0; i < 5;i++){
             h.add(pair_left[i]+" "+pair_right[i]);
@@ -21,5 +18,4 @@ public class hashsets {
 
     }
 
-    
 }
