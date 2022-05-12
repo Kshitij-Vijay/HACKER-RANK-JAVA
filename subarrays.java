8b1a51660232edfcd2fc5f77bca706893efc58ed
XITIJ <97301437+Kshitij-Vijay@users.noreply.github.com>
2022-05-12 23:07:59 +0530

    JAVA SUBARRAYS
    A SUB ARRAY IS A SUBSET OF THE ORIGINAL ARRAY CONTAING THE ELEMNTS IN THEIR ODERWISE MANNER

---
+++ subarrays.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class subarrays {
+    static Scanner in = new Scanner(System.in);
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void ma_solve() {
+        int l = in.nextInt();
+        int s, co = 0;
+        int arr[] = new int[l + 10];
+        for (int i = 0; i < l; i++) {
+            arr[i] = in.nextInt();
+        }
+        for (int j = 0; j <= l; j++) {
+            for (int k = 0; k <= l - j; k++) {
+                s = 0;
+                for (int i = 0; i < j; i++) {
+                    s = s + arr[i + k];
+                }
+                if (s < 0) {
+                    co++;
+                }
+            }
+
+        }
+        System.out.println(co);
+    }
+
+    public static void hackersolver() {
+        int n = scan.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = scan.nextInt();
+        }
+        scan.close();
+
+        int numNegativeSubarrays = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+
+                int sum = 0;
+
+                for (int k = i; k <= j; k++) {
+                    sum = sum + a[k];
+                }
+
+                if (sum < 0) {
+                    numNegativeSubarrays++;
+                }
+            }
+        }
+
+        System.out.println(numNegativeSubarrays);
+    }
+}
